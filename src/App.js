29d5d1468e66d7d3144d901bc045802c3de3e75a
Chaitanya Patel <chaitanya.patel@appnovation.com>
2021-12-13 17:56:05 -0500

    first commit...

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import "./App.css";
+import { client } from "./client";
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  const [counter,setCounter] = useState(0)
+  const [randomNumber,setRandomNumber] = useState(0)
+
+  // console.log("articles",articles)
+
+  useEffect(() => {
+    // client
+    //   .getEntries()
+    //   .then(res=>res?.items)
+    //   .then(data=>setArticles(data))
+      console.log("without counter depep")
+      
+  }, [counter,randomNumber]);
+
+  function handleClick(){
+    setCounter(counterState=>counterState+1)
+    if(counter>=10){
+      setRandomNumber(1)
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className="App">
+    {/* { articles?.map((article,index)=>(
+      <div className="container" key = {index}>
+        <header>
+          <div className="wrapper">
+            <span>
+          {article?.fields?.name}
+            </span>
+          </div>
+        </header>
+        <main>
+          <div className="wrapper">
+          <h1>Hello</h1>
+          </div>
+        </main>
+      </div>
+    )) } */}
+    <p>{counter}</p>
+    <p>{randomNumber}</p>
+
+    
+    <button onClick={handleClick}>Click here</button>
     </div>
   );
 }
